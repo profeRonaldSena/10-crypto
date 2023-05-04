69868ad0660c07a9e7a60788171e82281d4fb0a4
profeRonaldSena <ronald.barrera@cavirey.edu.co>
2023-05-04 09:14:18 -0500

    modified:   src/hooks/useMoneda.js

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -1,19 +1,38 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
+
+const LABEL = styled.label`
+ font-family: 'Bebas Neue', cursive;
+ color: #fff;
+ text-transform: uppercase;
+ font-weight: bold;
+ font-size: 2.4rem;
+ margin-top: 2rem;
+ display: block;
+`
+const SELECT = styled.select`
+ width: 100%;
+ display: block;
+ padding: 1rem;
+ -webkit-appearance: none;
+ border-radius: 10px;
+ border: none;
+`
 
 const useMoneda = (label, stateInitial, MONEDAS) => {
  // useState
  const [state, setState] = useState(stateInitial)
  const Seleccionar = () => (
   <>
-   <label>Moneda</label>
-   <select>
+   <LABEL>Elije tu Moneda</LABEL>
+   <SELECT onChange={(e) => setState(e.target.value)} value={state}>
     <option value=''>- Seleccione -</option>
     {MONEDAS.map((MONEDA) => (
      <option key={MONEDA.codigo} value={MONEDA.codigo}>
       {MONEDA.nombre}
      </option>
     ))}
-   </select>
+   </SELECT>
   </>
  )
  return [state, setState, Seleccionar]
