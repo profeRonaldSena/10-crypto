13aacb23516dcdf6fe2d296ab75f5bca5996a5da
profeRonaldSena <ronald.barrera@cavirey.edu.co>
2023-05-04 07:26:49 -0500

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/cryptomonedas.png 	modified:   src/index.css 	deleted:    src/logo.svg 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled'
+import imagen from './cryptomonedas.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const DIV = styled.div`
+ max-width: 900px;
+ margin: 0 auto;
+ @media (min-width: 992px) {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 2rem;
+ }
+`
+const IMAGEN = styled.img`
+ max-width: 100%;
+ margin-top: 5rem;
+`
+
+export default function App() {
+ return (
+  <DIV>
+   <div>
+    <IMAGEN src={imagen} alt='imagen de criptomonedas' />
+   </div>
+   <div></div>
+  </DIV>
+ )
 }
-
-export default App;
